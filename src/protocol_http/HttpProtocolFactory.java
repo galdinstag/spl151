a2f390d817f0a7f6a1a7d044d99c9b8430265539
galdinstag <galdinstag@post.bgu.ac.il>
2015-01-10 14:58:26 +0200

    gal'sTestAreDBest commit

--- src/protocol_http/HttpProtocolFactory.java
+++ src/protocol_http/HttpProtocolFactory.java
@@ -1,7 +1,14 @@
 package protocol_http;
 
+import protocol.ServerProtocol;
+import protocol.ServerProtocolFactory;
+
 /**
  * Created by airbag on 1/8/15.
  */
-public class HttpProtocolFactory {
+public class HttpProtocolFactory implements ServerProtocolFactory {
+    @Override
+    public ServerProtocol create() {
+        return null;
+    }
 }
